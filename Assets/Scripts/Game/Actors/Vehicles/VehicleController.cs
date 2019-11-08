3620572d1ecd49ff6c5943abc166f7b644b72f3a
Dmitri Kuznetsov <kna_int@mail.ru>
2019-11-08 22:05:19 +0200

    Moved elements around

---
+++ Assets/Scripts/Game/Actors/Vehicles/VehicleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VehicleControllerLocator))]
+public class VehicleController : MonoBehaviour
+{
+    //Not exposed
+
+    [Header("Constants")]
+    [ReadOnly]
+    [Tooltip("Logging source")]
+    public static string LOG_SOURCE = "VehicleController";
+
+    //Exposed
+
+    [Header("Locator")]
+
+    [Tooltip("Locator")]
+    public VehicleControllerLocator Locator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Game/Actors/Vehicles/VehicleControllerLocator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControllerLocator : MonoBehaviour
+{
+    [Tooltip("Components associated with this vehicle")]
+    public VehicleComponent[] Components;
+}
