4b212a42a41f54871b0fd341453dffd2a477ded4
Dmitri Kuznetsov <kna_int@mail.ru>
2020-01-09 20:37:37 +0200

    Updated block debug rendering

--- Assets/Scripts/Game/World/OpenWorldBlock.cs
+++ Assets/Scripts/Game/World/OpenWorldBlock.cs
@@ -17,24 +17,13 @@ public class OpenWorldBlock : MonoBehaviour
     [Tooltip("Current block position in the universe")]
     public long BlockZ;
 
-    [ReadOnly]
-    [Tooltip("Current block position in the universe")]
-    public LineRenderer DebugLineRenderer;
-
-    [ReadOnly]
-    [Tooltip("Current block position in the universe")]
-    public TextMesh DebugBlockName;
-
     public void UpdatePosition(long blockX, long blockZ)
     {
-        DebugLineRenderer = GetComponent<LineRenderer>();
-        DebugBlockName = GetComponent<TextMesh>();
-
         BlockX = blockX;
         BlockZ = blockZ;
 
         //make lines to match the size of the block
-        DebugLineRenderer.SetPositions(new Vector3[]
+        Locator.DebugLineRenderer.SetPositions(new Vector3[]
         {
             new Vector3( 0, 0, 0),
             new Vector3( OpenWorldController.Instance.BlockSize, 0, 0),
@@ -43,8 +32,8 @@ public class OpenWorldBlock : MonoBehaviour
             new Vector3( 0, 0, 0)
         });
 
-        DebugBlockName.color = UnityEngine.Color.white;
-        DebugBlockName.text = $"{BlockX},{BlockZ}";
+        Locator.DebugBlockName.color = UnityEngine.Color.white;
+        Locator.DebugBlockName.text = $"{BlockX},{BlockZ}";
 
         Log.Instance.Info(OpenWorldController.LOG_SOURCE, $"Block [{BlockX}, {BlockZ}] Updated");
     }
--- Assets/Scripts/Game/World/OpenWorldBlockLocator.cs
+++ Assets/Scripts/Game/World/OpenWorldBlockLocator.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class OpenWorldBlockLocator : MonoBehaviour
 {
+    [Tooltip("DEBUG - Used for rendering block bounds")]
+    public LineRenderer DebugLineRenderer;
 
+    [Tooltip("DEBUG - Block position")]
+    public TextMesh DebugBlockName;
 }
